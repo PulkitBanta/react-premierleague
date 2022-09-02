cf24e9b612e86188408f5b55e235ecefa7a44be5
Pulkit Banta <pulkitbanta01@gmail.com>
2022-09-02 08:12:31 +0530

    add util to parse and format data

---
+++ src/utils/__tests__/parseLeagueData.test.js
@@ -0,0 +1,148 @@
+import { parseLeagueData } from "../parseLegaueData";
+
+const mockLeagueData = [
+  {
+    score: { "Manchester United": null, Liverpool: null },
+    date: "2021-05-11T11:00:00",
+  },
+];
+
+it("should only create fixtures when the date is in future", () => {
+  expect(parseLeagueData(mockLeagueData)).toEqual({
+    "Manchester United": {
+      fixtures: [
+        {
+          date: "2021-05-11T11:00:00",
+          against: "Liverpool",
+        },
+      ],
+    },
+    Liverpool: {
+      fixtures: [
+        {
+          date: "2021-05-11T11:00:00",
+          against: "Manchester United",
+        },
+      ],
+    },
+  });
+});
+
+it("should only create fixtures if team score is null", () => {
+  expect(
+    parseLeagueData([{ ...mockLeagueData[0], date: "2021-05-01T11:00:00" }])
+  ).toEqual({
+    "Manchester United": {
+      fixtures: [
+        {
+          date: "2021-05-01T11:00:00",
+          against: "Liverpool",
+        },
+      ],
+    },
+    Liverpool: {
+      fixtures: [
+        {
+          date: "2021-05-01T11:00:00",
+          against: "Manchester United",
+        },
+      ],
+    },
+  });
+});
+
+it("should get all event details for both teams for one game", () => {
+  expect(
+    parseLeagueData([
+      {
+        score: { "Manchester United": 2, Liverpool: 1 },
+        date: "2021-05-01T11:00:00",
+      },
+    ])
+  ).toEqual({
+    "Manchester United": {
+      gameStatuses: ["W"],
+      totalPoints: 3,
+      totalGoalDifference: 1,
+      gamesPlayed: 1,
+      fixtures: [
+        {
+          date: "2021-05-01T11:00:00",
+          against: "Liverpool",
+        },
+      ],
+    },
+    Liverpool: {
+      gameStatuses: ["L"],
+      totalPoints: 0,
+      totalGoalDifference: -1,
+      gamesPlayed: 1,
+      fixtures: [
+        {
+          date: "2021-05-01T11:00:00",
+          against: "Manchester United",
+        },
+      ],
+    },
+  });
+});
+
+it("should get all event details all teams with more than one game having different outcome", () => {
+  expect(
+    parseLeagueData([
+      {
+        score: { "Manchester United": 2, Liverpool: 1 },
+        date: "2021-05-01T11:00:00",
+      },
+      {
+        score: { "Manchester United": 1, Liverpool: 3 },
+        date: "2021-05-04T11:00:00",
+      },
+      {
+        score: { "Manchester United": 2, Liverpool: 2 },
+        date: "2021-05-05T11:00:00",
+      },
+    ])
+  ).toEqual({
+    "Manchester United": {
+      gameStatuses: ["W", "L", "D"],
+      totalPoints: 4,
+      totalGoalDifference: -1,
+      gamesPlayed: 3,
+      fixtures: [
+        {
+          date: "2021-05-01T11:00:00",
+          against: "Liverpool",
+        },
+        {
+          date: "2021-05-04T11:00:00",
+          against: "Liverpool",
+        },
+        {
+          date: "2021-05-05T11:00:00",
+          against: "Liverpool",
+        },
+      ],
+    },
+    Liverpool: {
+      gameStatuses: ["L", "W", "D"],
+      totalPoints: 4,
+      totalGoalDifference: 1,
+      gamesPlayed: 3,
+      fixtures: [
+        {
+          date: "2021-05-01T11:00:00",
+          against: "Manchester United",
+        },
+        {
+          date: "2021-05-04T11:00:00",
+          against: "Manchester United",
+        },
+        {
+          date: "2021-05-05T11:00:00",
+          against: "Manchester United",
+        },
+      ],
+    },
+  });
+});
---
+++ src/utils/parseLegaueData.js
@@ -0,0 +1,96 @@
+// import { compareAsc } from "date-fns";
+const { compareAsc } = require("date-fns");
+
+const CURRENT_DATE = "5 May 2021 14:00:00";
+
+/**
+ *
+ * @param {*} score - difference in goals of two teams
+ * @returns array with game state and points
+ */
+function getGameStatusAndPoints(score) {
+  switch (true) {
+    case score === 0:
+      return ["D", 1];
+    case score < 0:
+      return ["L", 0];
+    case score > 0:
+      return ["W", 3];
+  }
+}
+
+/**
+ *
+ * @param {*} teamName - name of the team
+ * @param {*} teamScore - goals scored by the team
+ * @param {*} opponentName - name of the opponent team
+ * @param {*} opponentScore - goals scored by the opponent team
+ * @param {*} dateOfEvent - date on which event was/is scheduled
+ * @param {*} previousState - object with previous details for the team
+ * @returns Object with gameStatuses, totalPoints, totalGoalDifference & fixtures
+ */
+function getEventDetails(
+  teamName,
+  teamScore,
+  opponentName,
+  opponentScore,
+  dateOfEvent,
+  previousState
+) {
+  const goalDifference = teamScore - opponentScore,
+    [gameState, teamPoints] = getGameStatusAndPoints(goalDifference);
+  return {
+    ...previousState,
+    // games with score as null are not counted for Draw (assuming game didn'nt occurred) & are only added for the fixtures
+    // games which are in future are only considered for the fixtures
+    ...(teamScore !== null &&
+    opponentScore !== null &&
+    compareAsc(new Date(dateOfEvent), new Date(CURRENT_DATE)) === -1
+      ? {
+          gameStatuses: [...(previousState?.gameStatuses || []), gameState],
+          totalPoints: (previousState?.totalPoints || 0) + teamPoints,
+          totalGoalDifference:
+            (previousState?.totalGoalDifference || 0) + goalDifference,
+          gamesPlayed: (previousState?.gamesPlayed || 0) + 1,
+        }
+      : {}),
+    fixtures: [
+      ...(previousState?.fixtures || []),
+      { date: dateOfEvent, against: opponentName },
+    ],
+  };
+}
+
+/**
+ *
+ * @param {*} leageDataList List of the league data
+ * @returns formatted object with the fixtures, gameStatuses, totalPoints, totalGoalDifference, games played
+ */
+export const parseLeagueData = (leageDataList) => {
+  // parse and format the data
+  return leageDataList.reduce((acc, leagueData) => {
+    // getting team name and score from score object
+    const [firstTeamName, secondTeamName] = Object.keys(leagueData.score);
+    const [firstTeamScore, secondTeamScore] = Object.values(leagueData.score);
+
+    // assigning values in the object with key name as team namme
+    // keeping fixtures as the match played and status as the status of each game (W: Win, L: Loss, D: Draw)
+    acc[firstTeamName] = getEventDetails(
+      firstTeamName,
+      firstTeamScore,
+      secondTeamName,
+      secondTeamScore,
+      leagueData.date,
+      acc[firstTeamName] || {}
+    );
+    acc[secondTeamName] = getEventDetails(
+      secondTeamName,
+      secondTeamScore,
+      firstTeamName,
+      firstTeamScore,
+      leagueData.date,
+      acc[secondTeamName] || {}
+    );
+    return acc;
+  }, {});
+};
